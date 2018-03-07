6e13a65b4a46f554a244462cf12fa841e9ba42f9
Genius1996 <wab0212@gmail.com>
2018-03-07 16:34:34 -0600

    Added Flickering Light Script

---
+++ Mansion of Memories/Assets/Scripts/FlickerLights.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLights : MonoBehaviour {
+
+    public Light lightObject;
+
+
+	// Use this for initialization
+	void Start () {
+        lightObject = GetComponent<Light>();
+	}
+
+	void Update () {
+        if(Random.value > 0.9)
+        {
+            if (lightObject.enabled == true)
+            {
+                lightObject.enabled = false;
+            }
+            else
+                lightObject.enabled = true;
+        }
+	}
+}
