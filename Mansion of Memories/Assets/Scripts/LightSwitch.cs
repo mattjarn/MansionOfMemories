dd9751237cc8d62c982f1a5d6f9c0b665f833406
Genius1996 <wab0212@gmail.com>
2018-03-19 13:15:15 -0500

    Added interactivity to the light switch.
    Tried to add interactivity to the light switch.

---
+++ Mansion of Memories/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class LightSwitch : MonoBehaviour {
+    AudioSource lightOnSound;
+    AudioSource lightOffSound;
+
+    public bool lighted;
+
+    public Light[] lights;
+    private Light[] flickeringLights;
+    private Light[] notFlickering;
+    private int fLights;
+    private int nLights;
+
+    private VRTK_InteractableObject vrObj;
+
+    private void Awake()
+    {
+        vrObj = GetComponent<VRTK_InteractableObject>();
+        AudioSource[] lightSounds = GetComponents<AudioSource>();
+        lightOnSound = lightSounds[0];
+        lightOffSound = lightSounds[1];
+
+        flickeringLights = new Light[lights.Length];
+        notFlickering = new Light[lights.Length];
+        fLights = 0;
+        nLights = 0;
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i].enabled = false;
+            if (lights[i].gameObject.tag == "Flicker")
+            {
+                flickeringLights[fLights] = lights[i];
+                fLights++;
+            }
+            else
+            {
+                notFlickering[nLights] = lights[i];
+                nLights++;
+            }
+        }
+        lighted = false;
+    }
+    
+    void Update()
+    {
+        if (this.vrObj.IsUsing() == true)
+        {
+            lighted = !lighted;
+        }
+
+        if (lighted)
+        {
+            for (int i = 0; i < flickeringLights.Length; i++)
+            {
+                if (flickeringLights[i] != null)
+                {
+                    if (Random.value > 0.9)
+                    {
+                        if (flickeringLights[i].enabled != true)
+                        {
+                            flickeringLights[i].enabled = true;
+                        }
+                        else
+                        {
+                            flickeringLights[i].enabled = false;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < flickeringLights.Length; i++)
+            {
+                if (flickeringLights[i] != null)
+                {
+                    flickeringLights[i].enabled = false;
+                }
+            }
+        }
+    }
+}
