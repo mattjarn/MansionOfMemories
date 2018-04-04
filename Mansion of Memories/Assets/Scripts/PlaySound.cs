90f1abd8d87d7693f9de214fb49716e4e3aa8cb8
Genius1996 <wab0212@gmail.com>
2018-04-04 08:53:57 -0500

    Fixes and additions
    Fixed Items falling throught the floor.
    
    Added a script to play a sound when entering a trigger.

---
+++ Mansion of Memories/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    public AudioSource sound;
+
+	// Use this for initialization
+	void Awake () {
+        sound = GetComponent<AudioSource>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        sound.Play();
+    }
+}
