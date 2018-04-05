6d4ebca44fcdac374df8b0562a51519fa89f8039
Matt Jarnevic <mattjarn@gmail.com>
2018-04-05 12:20:17 -0500

    Changed main scene lighting in hallway, added new script

---
+++ Mansion of Memories/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public Light lt;
+
+    public AudioSource fizzle;
+
+    private void Start()
+    {
+        lt = GetComponent<Light>();  
+    }
+
+    private void Update()
+    {
+
+            if (Random.value > 0.9)
+            {
+                if (lt.enabled != true)
+                {
+                    lt.enabled = true;
+                    if (fizzle != null)
+                    {
+                        fizzle.Play();
+                    }
+                }
+                else
+                {
+                    lt.enabled = false;
+                }
+            }
+    }
+
+}
