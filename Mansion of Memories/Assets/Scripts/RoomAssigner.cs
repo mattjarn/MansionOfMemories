6458bced3c548676bb057b71d7cf3672349d30dd
Matt Jarnevic <mattjarn@gmail.com>
2018-04-12 13:46:43 -0500

    Added new room assigner script for new rooms

---
+++ Mansion of Memories/Assets/Scripts/RoomAssigner.cs
@@ -0,0 +1,59 @@
+﻿//Author: Matt Jarnevic
+//Date Created: 3/18/18
+//Date Modified: 3/28/18
+//Modified by: Matt Jarnevic
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RoomAssigner : MonoBehaviour
+{
+    private GameObject[] placementPoints;
+    public List<GameObject> rooms;
+
+    private void Awake()
+    {
+        placementPoints = GameObject.FindGameObjectsWithTag("Placeable");
+
+        for (int i = 0; i < placementPoints.Length; i++)
+        {
+            Vector3 position = new Vector3(placementPoints[i].transform.position.x, placementPoints[i].transform.position.y, placementPoints[i].transform.position.z);
+            int rotation = (int)(placementPoints[i].transform.eulerAngles.y / 90);
+
+            switch (rotation)
+            {
+                case 0:
+                    position.x -= 0.305f;
+                    position.y += 1.1f;
+                    position.z -= 10.0f;
+                    break;
+                case 1:
+                    position.x -= 10.0f;
+                    position.y += 1.1f;
+                    position.z += 0.305f;
+                    break;
+                case 2:
+                    position.x += 0.305f;
+                    position.y += 1.1f;
+                    position.z += 10.0f;
+                    break;
+                case 3:
+                    position.x += 10.0f;
+                    position.y += 1.1f;
+                    position.z -= 0.305f;
+                    break;
+
+            }
+
+            int index = Random.Range(0, rooms.Count);
+            Instantiate(rooms[index], position, placementPoints[i].transform.rotation);
+            rooms.RemoveAt(index);
+        }
+
+
+    }
+}
+
+
