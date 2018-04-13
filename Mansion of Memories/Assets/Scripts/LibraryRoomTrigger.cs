e44ade14ef0643eaa093e510b77a7637c1aa9d14
Matt Jarnevic <mattjarn@gmail.com>
2018-04-12 23:31:53 -0500

    Created a cool secret

---
+++ Mansion of Memories/Assets/Scripts/LibraryRoomTrigger.cs
@@ -0,0 +1,42 @@
+﻿//Author: Matt Jarnevic
+//Date Created: 4/12/18
+//Date Modified: 
+//Modified by:
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class LibraryRoomTrigger : MonoBehaviour
+{
+    [Header("Set in the Inspector")]
+    //What timeline you want to trigger
+    public PlayableDirector timeline;
+    //The clip you want to play if you want sound
+    public AudioSource clip;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    //Stops timeline on player exit
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == "SpecialBook")
+        {
+            timeline.Pause();
+            clip.Pause();
+        }
+    }
+
+    //Starts/resumes timeline when player enters. 
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "SpecialBook")
+        {
+            timeline.Play();
+            clip.Play();
+        }
+
+    }
+}
